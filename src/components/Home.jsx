68d62e331b1bc229672c05908077d2f951161291
luca1777 <luca.alexandru17@yahoo.com>
2023-08-26 18:13:52 +0300

    modify navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
         {/* container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className="text-pink-600">Hi, my name is</p>
+            <p className="text-pink-600 font-mulish">Hi, my name is</p>
             <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Luca Alexandru</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm Front-End developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>A highly motivated and detail-oriented junior front-end developer with a passion for crafting intuitive user experiences.Adept at collaborating in team environments and always eager to learn the latest web technologies to stay at the forefront of front-end development.</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import Logo from "../assets/logo1.png";
 import {Link} from "react-scroll";
 
 export const Navbar = () => {
@@ -8,12 +7,12 @@ export const Navbar = () => {
     const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed bg-[#0a192f] w-full h-[80px] flex justify-between items-center px-4 text-gray-300">
+    <div className="fixed bg-[#FFFFFF] w-full h-[80px] flex justify-between items-center px-4 text-[#2D2E32]">
       <div>
-        <img className="logo-img" src={Logo} alt="logo img"></img>
+        <h3 className="text-[#2D2E32] text-xl font-bold mx-[1em]">Luca.dev</h3>
       </div>
       {/* menu */}
-      <ul className="hidden md:flex">
+      <ul className="hidden md:flex font-semibold">
         <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
         <li><Link to="about" smooth={true} duration={500}>About</Link></li>
         <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
@@ -21,11 +20,11 @@ export const Navbar = () => {
         <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
       {/* hamburger */}
-      <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
+      <div onClick={handleClick} className="md:hidden z-10 cursor-pointer text-[#2D2E32] hover:text-blue-500 text-xl">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* mobile menu */}
-      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
+      <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#FFFFFF] flex flex-col justify-center items-center"}>
         <li className="py-6 text-4xl"><Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link></li>
         <li className="py-6 text-4xl"><Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link></li>
         <li className="py-6 text-4xl"><Link onClick={handleClick} to="skills" smooth={true} duration={500} offset={-40}>Skills</Link></li>
