693c3f583e07a8ba2527d36401820658241a4d08
luca1777 <luca.alexandru17@yahoo.com>
2023-08-26 16:00:22 +0300

    added scroll function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name="contact" className='w-full h-full bg-[#0a192f] flex justify-center items-center p-4'>
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 pr-2 text-gray-300'>Contact</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo1.png";
+import {Link} from "react-scroll";
 
 export const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -13,11 +14,11 @@ export const Navbar = () => {
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+        <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
+        <li><Link to="work" smooth={true} duration={500}>Projects</Link></li>
+        <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
       {/* hamburger */}
       <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
@@ -25,11 +26,11 @@ export const Navbar = () => {
       </div>
       {/* mobile menu */}
       <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Projects</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="skills" smooth={true} duration={500} offset={-40}>Skills</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="work" smooth={true} duration={500} offset={-80}>Projects</Link></li>
+        <li className="py-6 text-4xl"><Link onClick={handleClick} to="contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
     </div>
   );
