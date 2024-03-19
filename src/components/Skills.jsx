4703932ae245c1a6beeee3bae41723f9e43dec22
luca1777 <luca.alexandru17@yahoo.com>
2024-03-19 10:12:40 +0200

    changed ui for skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ import Figma from "../assets/apps-figma.svg";
 
 export const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#F9F9F9] text-[#2d2e32]">
+    <div name="skills" className="w-full py-10 bg-[#F9F9F9] text-[#2d2e32]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="text-center md:text-left">
           <p className=" text-4xl font-bold inline border-b-4 border-[#147EFB] pr-2">
